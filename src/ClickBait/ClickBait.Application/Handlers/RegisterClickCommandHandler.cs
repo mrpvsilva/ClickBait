734aaf34f6c743cc1b53560ae745629b96f2b523
Paulo Silva <mrpvsilva@gmail.com>
2022-04-22 23:41:26 -0300

    loggin register click

--- src/ClickBait/ClickBait.Application/Handlers/RegisterClickCommandHandler.cs
+++ src/ClickBait/ClickBait.Application/Handlers/RegisterClickCommandHandler.cs
@@ -1,26 +1,39 @@
 ﻿using ClickBait.Application.Commands;
 using ClickBait.Application.Services;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace ClickBait.Application.Handlers
 {
     public class RegisterClickCommandHandler : IRequestHandler<RegisterClickCommand>
     {
         private readonly IClickService _clickService;
+        private readonly ILogger<RegisterClickCommandHandler> _logger;
 
-        public RegisterClickCommandHandler(IClickService clickService)
+        public RegisterClickCommandHandler(IClickService clickService, ILogger<RegisterClickCommandHandler> logger)
         {
             _clickService = clickService;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(RegisterClickCommand request, CancellationToken cancellationToken)
         {
-            await _clickService.Register(new Domain.Entities.Click
+            try
             {
-                PostId = request.PostId
-            });
+                await _clickService.Register(new Domain.Entities.Click
+                {
+                    PostId = request.PostId
+                });
 
-            return Unit.Value;
+                _logger.LogInformation($"Click registered {request.PostId} successfully");
+
+                return Unit.Value;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error on register click {request.PostId}");
+                throw;
+            }
         }
     }
 }
